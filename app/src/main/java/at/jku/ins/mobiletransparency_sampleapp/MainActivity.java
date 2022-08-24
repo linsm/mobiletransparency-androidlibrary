a51b8a2adbd8e7a61664972597c6022f109a8246
Mario Lins <mario.lins25@gmail.com>
2022-08-24 15:21:29 +0200

    make tree id selectable

--- app/src/main/java/at/jku/ins/mobiletransparency_sampleapp/MainActivity.java
+++ app/src/main/java/at/jku/ins/mobiletransparency_sampleapp/MainActivity.java
@@ -1,18 +1,23 @@
 package at.jku.ins.mobiletransparency_sampleapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import androidx.appcompat.app.AppCompatActivity;
+import java.util.ArrayList;
+import java.util.List;
 import at.jku.ins.mobiletransparency.MobileTransparencyClient;
 import at.jku.ins.mobiletransparency.TransparencyCallback;
 import at.jku.ins.mobiletransparency.models.Tree;
 import at.jku.ins.mobiletransparency.models.TreeInformation;
 import at.jku.ins.mobiletransparency_sampleapp.databinding.ActivityMainBinding;
 
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private MobileTransparencyClient mobileTransparencyClient;
+    private String selectedTreeId;
 
 
     @Override
@@ -25,9 +30,20 @@ public class MainActivity extends AppCompatActivity {
         mobileTransparencyClient.getAvailableTrees(new TransparencyCallback() {
             @Override
             public void onSuccess(Tree treeInformation) {
-
+                List<String> treeIds = new ArrayList<>();
+                for (TreeInformation item: treeInformation.getTreeInformation()) {
+                    treeIds.add(String.valueOf(item.getTreeId()));
+                }
+                ArrayAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.treeid_item,treeIds);
+                AutoCompleteTextView treeIdField = findViewById(R.id.treeIdAutoComplete);
+                treeIdField.setAdapter(adapter);
+                treeIdField.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        selectedTreeId = treeIds.get(position);
+                    }
+                });
             }
-
             @Override
             public void onFailure() {
                 int i = 0;
--- mobiletransparency/src/main/java/at/jku/ins/mobiletransparency/models/Tree.java
+++ mobiletransparency/src/main/java/at/jku/ins/mobiletransparency/models/Tree.java
@@ -2,8 +2,6 @@ package at.jku.ins.mobiletransparency.models;
 
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Tree {
@@ -11,5 +9,13 @@ public class Tree {
     @Expose
     @SerializedName("tree")
     private List<TreeInformation> treeInformation;
+
+    Tree(List<TreeInformation> treeInformation) {
+        this.treeInformation = treeInformation;
+    }
+
+    public List<TreeInformation> getTreeInformation() {
+        return treeInformation;
+    }
 
 }
